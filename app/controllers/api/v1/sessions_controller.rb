636b7d686b9b4cb892c27c8ad1dd046712f10723
Gokhan <gokhan@sylow.net>
2024-08-18 18:17:43 +0300

    Use cookie to fetch auth data

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,4 +14,15 @@ class Api::V1::SessionsController < ApplicationController
                    exp: time.strftime("%m-%d-%Y %H:%M"),
                    email: user.email }, status: :ok
   end
+
+  def refresh
+    token = params[:token]
+    data = JWT.decode(token, nil, false).first
+    if user = User.find_by(id: data["user_id"])
+      decoded = JWT.decode(token, nil, false).first.merge({ "token" => token, "email": user.email, "exp" => Time.at(data["exp"]).to_datetime.strftime("%m-%d-%Y %H:%M") })
+      render json: decoded, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :sessions
+      resources :sessions do
+        get :refresh, on: :collection
+      end
       resources :users
     end
   end
