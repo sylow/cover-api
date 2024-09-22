5b8b5ea9b72b42e1c3d476c0464284bfebda628c
Gokhan <gokhan@sylow.net>
2024-09-22 22:12:50 +0300

    Update cors to handle api calls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow jobcraftsman.com and its subdomains
-    allow do
-      origins 'https://jobcraftsman.com', 'https://www.jobcraftsman.com'
-      resource '/api/v1/*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+    origins 'https://jobcraftsman.com', 'https://www.jobcraftsman.com'
+    resource '/api/v1/*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 
-    if Rails.env.development?
-      origins 'http://localhost:3001'
+  # Keep the development configuration if needed
+  if Rails.env.development?
+    allow do
+      origins 'http://localhost:3000'
       resource '/api/v1/*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
