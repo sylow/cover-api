0bc82dad88793faf7a6532efc70eac598f70b2c2
Gokhan <gokhan@sylow.net>
2024-09-22 23:55:06 +0300

    Remove a comment in stripe file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,7 +4,6 @@ StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET'] # e.g. whsec_...
 class CheckoutCompleted
   def call(event)
     object = event.data.object
-    # raise "#{object.client_reference_id} - #{object.amount_subtotal}".inspect
     Purchases::Paid.run!(id: Integer(object.client_reference_id), price_cents: Integer(object.amount_subtotal))
   end
 end
