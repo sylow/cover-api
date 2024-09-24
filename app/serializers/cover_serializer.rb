4a080b42129607d0c1c3c65a20d5feb3f610495e
Gokhan <gokhan@sylow.net>
2024-09-24 11:07:51 +0300

    Add resume content to serializer to enable rendering on frontend

--- app/serializers/cover_serializer.rb
+++ app/serializers/cover_serializer.rb
@@ -1,5 +1,5 @@
 class CoverSerializer < BaseSerializer
-  attributes :id, :job_description, :resume_title, :resume, :cover, :aasm_state, :created_at
+  attributes :id, :job_description, :resume_title, :resume, :resume_content, :cover, :aasm_state, :created_at
 
   def resume_title
     object.resume.title
